0749655eca7e43c97e347694c0c27d5440f018dd
MartinRuan <632303445@qq.com>
2012-04-14 05:16:38 -0700

    add insertsort func for single_list

--- single_list.c
+++ single_list.c
@@ -1,5 +1,71 @@
-typedef struct _single_node
+#include "single_list.h"
+struct _single_node
 {
 	void *p_data;
-	struct _single_node
-}single_node, p_single_node;
+	struct _single_node p_next;
+};
+/**
+ * \bref use for sort the single_list
+ * \param  
+ * \return 
+ */
+int slist_insertsort(single_list *list, compare_func fn)
+{
+	single_node *p_node;
+	single_node *p_prenode;
+	single_node *tmp;
+	single_node *p_curnode;
+	if(single_list == NULL || fn == NULL)
+	{
+		return -1;
+	}
+	
+	p_node = list->p_head;
+	p_prenode = NULL;
+	
+	if(p_node == NULL)
+	{
+		return 0;
+	}
+	
+	while(p_node->p_next != NULL)
+	{
+		tmp = list->p_head;
+		p_prenode = NULL;
+		while(tmp != p_node->next)
+		{
+			if((*fn)(p_node->p_next->p_data, tmp->p_data) < 0)
+			{
+				p_curnode = p_node->next;
+				if(p_prenode != NULL)
+				{
+					
+				}
+				else
+				{
+					p_node->p_next = p_node->p_next->p_next;
+					list->p_head = p_curnode;
+					p_curnode->p_next = tmp;
+				}	
+		
+				if(p_curnode == list->p_tail)
+				{
+					list->p_tail = p_node;
+				}
+				break;
+			}
+			p_prenode = tmp;
+			tmp = tmp->p_next;
+		}
+		if(tmp == p_node->p_next)
+		{
+			p_node = p_node->p_next;
+		}
+		else
+		{
+			
+		}
+	}
+	return 0;
+}
+
--- single_list.h
+++ single_list.h
@@ -1,5 +1,6 @@
 #ifndef __SINGLE_LIST_H__
 #define __SINGLE_LIST_H__
+#define TEST 1
 typedef unsigned int uint;
 typedef int(*compare_func)(void *p_data1, void *p_data2);
 typedef int(*destroy_func)(void *p_data);
@@ -10,5 +11,14 @@ typedef struct _single_list
 	single_node *p_tail;
 	single_node *p_cur;
 	uint count;
-};
+}single_list;
+single_list *slist_create(void);
+void slist_destroy(single_list *list, destroy_func fn);
+int insert_head(single_list *list, void *data);
+int insert_tail(single_list *list, void *data);
+void *head_pop(single_list *list);
+void *head_get(single_list *list);
+void *tail_get(single_list *list);
+void *tail_pop(single_list *list);
+void *count_get(single_list *list);
 #endif
