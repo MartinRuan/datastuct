441dbc29d4794f07868d68bcb5633b7a92e9f5c4
MartinRuan <632303445@qq.com>
2013-05-07 09:12:08 +0800

    实现二叉树基本接口

--- src/bitree.c
+++ src/bitree.c
@@ -1,6 +1,10 @@
 #include <stdlib.h>
 #include "bitree.h"
 
+/*
+ * @tree:
+ * @destroy:
+ */
 void bitree_init(bitree_t *tree, void (*destory)(void *data))
 {
     if (NULL == tree) {
@@ -11,6 +15,9 @@ void bitree_init(bitree_t *tree, void (*destory)(void *data))
     tree->root = NULL;
 }
 
+/*
+ * @tree:
+ */
 void bitree_destory(bitree_t *tree)
 {
     if (NULL == tree) {
@@ -20,6 +27,10 @@ void bitree_destory(bitree_t *tree)
     memset(tree, 0, sizeof(*tree));
 }
 
+/*
+ * @tree:
+ * @node:
+ */
 void bitree_rem_left(bitree_t *tree, bitree_node_t *node)
 {
     bitree_node_t **position;
@@ -46,6 +57,10 @@ void bitree_rem_left(bitree_t *tree, bitree_node_t *node)
     }
 }
 
+/*
+ * @tree:
+ * @node:
+ */
 void bitree_rem_right(bitree_t *tree, bitree_node_t *node)
 {
     bitree_node_t **position;
@@ -72,7 +87,109 @@ void bitree_rem_right(bitree_t *tree, bitree_node_t *node)
     }
 }
 
+/*
+ * @tree:
+ * @node:
+ * @data:
+ */
 int bitree_ins_left(bitree_t *tree, bitree_node_t *node, const void *data)
 {
+    bitree_node_t *new_node, **position;
+    if (NULL == tree || NULL == data) {
+        return -1;
+    }
+
+    if (NULL == node) {
+        if (BITREE_SIZE(tree) != 0) {
+            return -1;
+        }
+        position = &tree->root;
+    } else {
+        if (NULL != BITREE_LEFT(node)) {
+            return -1;
+        }
+        position = &node->left;
+    }
+
+    new_node = (bitree_node_t *)malloc(sizeof(*new_node));
+    if (NULL == new_node) {
+        return -1;
+    }
+
+    new_node->data = data;
+    new_node->left = NULL;
+    new_node->right = NULL;
+    *position = new_node;
+    tree->size++;
     
+    return 0;
+}
+
+/*
+ * @tree:
+ * @node:
+ * @data:
+ */
+int bitree_ins_right(bitree_t *tree, bitree_node_t *node, const void *data)
+{
+    bitree_node_t *new_node, **position;
+    if (NULL == tree || NULL == data) {
+        return -1;
+    }
+
+    if (NULL == node) {
+        if (BITREE_SIZE(tree) != 0) {
+            return -1;
+        }
+        position = &tree->root;
+    } else {
+        if (NULL != BITREE_RIGHT(node)) {
+            return -1;
+        }
+        position = &node->right;
+    }
+
+    new_node = (bitree_node_t *)malloc(sizeof(*new_node));
+    if (NULL == new_node) {
+        return -1;
+    }
+
+    new_node->data = data;
+    new_node->left = NULL;
+    new_node->right = NULL;
+    *position = new_node;
+    tree->size++;
+    
+    return 0;
+}
+
+/*
+ * @merge:
+ * @right:
+ * @left:
+ * @data:
+ */
+int bitree_merge(bitree_t *merge, bitree_t *right, bitree_t *left, const void *data)
+{
+    if (NULL == merge || NULL == right || NULL == left) {
+        return -1;
+    }
+    bitree_init(merge, left->destroy);
+
+    if (bitree_ins_left(merge, NULL, data) != 0) {
+        bitree_destory(merge);
+        return -1;
+    }
+
+    BITREE_ROOT(merge)->left = BITREE_ROOT(left);
+    BITREE_ROOT(merge)->right = BITREE_ROOT(right);
+
+    merge->size = merge->size + BITREE_SIZE(left) + BITREE_SIZE(right);
+
+    left->root = NULL;
+    left->size = 0;
+    right->root = NULL;
+    right->size = 0;
+
+    return 0;
 }
--- src/bitree.h
+++ src/bitree.h
@@ -1,8 +1,12 @@
 #ifndef _BITREE_H_
 #define _BITREE_H_
 
-#define BITREE_SIZE(tree) (tree->size)
-
+#define BITREE_SIZE(tree)   ((tree)->size)
+#define BITREE_LEFT(node)   ((node)->left)
+#define BITREE_RIGHT(node)  ((node)->right)
+#define BITREE_ROOT(tree)   ((tree)->root)
+#define BITREE_IS_LEFT(node) ((node)->left == NULL && (node)->right == NULL)
+#define BITREE_IS_EOB(node) ((node) == NULL)
 
 typedef struct bitree_node_s {
     void *data;
@@ -19,6 +23,9 @@ typedef struct bitree_s {
 
 void bitree_init(bitree_t *tree, void (*destory)(void *data));
 void bitree_destory(bitree_t *tree);
+int bitree_ins_left(bitree_t *tree, bitree_node_t *node, void *data);
+int bitree_ins_right(bitree_t *tree, bitree_node_t *node, void *data);
+int bitree_merge(bitree_t *merge, bitree_node_t *right, const void *data);
 void bitree_rem_left(bitree_t *tree, bitree_node_t *node);
 void bitree_rem_right(bitree_t *tree, bitree_node_t *node);
 
